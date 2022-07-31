5b126114c5b4956272115c88291ec41923cf3a27
imshahrukh <sk902224@gmail.com>
2022-07-31 22:27:37 +0500

    show and post list page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
+import {Nav} from "./components/nav"
+import { useEffect ,useState} from 'react';
+import { getAllPosts } from './api';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'  
+import {ShowPost} from "./pages/postShow"
+import {PostPage} from "./pages/postsPage"
+import { Test } from './pages/test';
+import PostState from "./context/PostState"
+function App() {           
+ 
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <PostState>
+      <Router>
+        <div className="App">   
+
+          {/* Show all the posts */}
+          <div className='mx-3'>
+            <Nav/>
+            <div className='mt-4'></div>  
+            <div className='flex justify-center flex-col items-center space-y-4'>
+              <Routes>
+                <Route path="/" element={<PostPage/>}/>
+                <Route path="/posts/:id" element={<ShowPost/>}/>
+                <Route path="/test" element={<Test/>}>
+
+              </Route>
+            </ Routes>
+            </div>
+          </div>
+        </div>
+      </Router>
+    </PostState>
   );
 }
 
---
+++ src/pages/postsPage.jsx
@@ -0,0 +1,23 @@
+import  {useState,useEffect} from "react"
+import { getAllPosts } from "../api"
+import {PostCard} from "./../components/postCard"
+
+export const PostPage = ()=>{
+    const [posts,setPost] = useState([])
+
+    const getAllData = async()=>{
+      const data = await getAllPosts()
+      setPost(data)
+ 
+    }
+    useEffect(()=>{
+        getAllData()
+      },[])
+
+    return <>
+         {
+            !!posts? posts.map(el=>(<PostCard posts={el}/>)):<div className="text-white">No Record Found</div>
+              
+            }
+    </>
+}
