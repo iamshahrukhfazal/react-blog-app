44c16a994ca850257249cb27d49e912134c954b9
imshahrukh <sk902224@gmail.com>
2022-07-31 22:33:33 +0500

    init

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import {Nav} from "./components/nav"
-import { useEffect ,useState} from 'react';
-import { getAllPosts } from './api';
+
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'  
 import {ShowPost} from "./pages/postShow"
 import {PostPage} from "./pages/postsPage"
-import { Test } from './pages/test';
-import PostState from "./context/PostState"
+
 function App() {           
  
  
   return (
-    <PostState>
+ 
       <Router>
         <div className="App">   
 
@@ -32,7 +30,7 @@ function App() {
           </div>
         </div>
       </Router>
-    </PostState>
+
   );
 }
 
